ecef8c48e3e15f84fa4299712ef7022e2d064a03
Matheus Braynner <m.braynner@outlook.com>
2022-03-12 23:11:58 -0300

    Config instantiation

---
+++ src/main/java/com/matheus/braynner/workshopmongo/config/Instantiation.java
@@ -0,0 +1,29 @@
+package com.matheus.braynner.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.matheus.braynner.workshopmongo.domain.User;
+import com.matheus.braynner.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+	}
+}
