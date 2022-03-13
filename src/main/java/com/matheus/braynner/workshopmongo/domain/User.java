42c07a0d93da775de2a916d1624b70483e5774d6
Matheus Braynner <m.braynner@outlook.com>
2022-03-12 22:46:24 -0300

    Entity User and REST working

---
+++ src/main/java/com/matheus/braynner/workshopmongo/domain/User.java
@@ -0,0 +1,40 @@
+package com.matheus.braynner.workshopmongo.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private String id;
+	private String name;
+	private String email;
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+}
