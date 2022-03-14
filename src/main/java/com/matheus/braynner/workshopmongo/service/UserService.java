d753ad8fb1a52b5d18695e51ccfd889bca5e578f
Matheus Braynner <m.braynner@outlook.com>
2022-03-13 21:58:33 -0300

    Delete Mapping

--- src/main/java/com/matheus/braynner/workshopmongo/service/UserService.java
+++ src/main/java/com/matheus/braynner/workshopmongo/service/UserService.java
@@ -13,23 +13,29 @@ import com.matheus.braynner.workshopmongo.service.exception.ObjectNotFoundExcept
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repository;
-	
+
 	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> user = repository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
-	
+
 	public User insert(User obj) {
 		return repository.insert(obj);
 	}
-	
+
+	public void delete(String id) {
+		if (!(findById(id) == null)) {
+			repository.deleteById(id);
+		}
+	}
+
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
