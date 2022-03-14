049f61e85cdad62fdbea96110b5afaeae2c1b88a
Matheus Braynner <m.braynner@outlook.com>
2022-03-13 21:44:10 -0300

    Post Mapping

--- src/main/java/com/matheus/braynner/workshopmongo/service/UserService.java
+++ src/main/java/com/matheus/braynner/workshopmongo/service/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.matheus.braynner.workshopmongo.domain.User;
+import com.matheus.braynner.workshopmongo.dto.UserDTO;
 import com.matheus.braynner.workshopmongo.repository.UserRepository;
 import com.matheus.braynner.workshopmongo.service.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> user = repository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
