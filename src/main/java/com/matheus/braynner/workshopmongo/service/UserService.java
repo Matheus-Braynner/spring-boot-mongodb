dd252fac0e09a2bbdb3a8a924b03dcf99bf33e54
Matheus Braynner <m.braynner@outlook.com>
2022-03-13 21:26:29 -0300

    Get Id and handler exception

--- src/main/java/com/matheus/braynner/workshopmongo/service/UserService.java
+++ src/main/java/com/matheus/braynner/workshopmongo/service/UserService.java
@@ -1,12 +1,14 @@
 package com.matheus.braynner.workshopmongo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.matheus.braynner.workshopmongo.domain.User;
 import com.matheus.braynner.workshopmongo.repository.UserRepository;
+import com.matheus.braynner.workshopmongo.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repository.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
 }
